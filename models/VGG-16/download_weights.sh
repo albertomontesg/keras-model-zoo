d5395767a500f07572d6831d55da5b66b722f6dd
Alberto Montes <alm59321@gmail.com>
2016-04-05 11:22:15 +0200

    Update the VGG weights file link

--- models/VGG-16/download_weights.sh
+++ models/VGG-16/download_weights.sh
@@ -1,1 +1,1 @@
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&confirm=Pup1&id=0Bz7KyqmuGsilT0J5dmRCM0ROVHc' -O vgg-16_weights.h5
+wget --no-check-certificate 'https://www.dropbox.com/s/u3w3ud3hlp11nwt/vgg16_weights.h5?dl=0' -O vgg-16_weights.h5
